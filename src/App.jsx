59579fe346c8959e0fc834944fb78b7d65209a12
AlsoFabio <minette_sas@hotmail.com>
2022-11-25 14:16:59 -0300

    xd

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+<<<<<<< HEAD
 import React,{useState, useContext, useReducer} from 'react';
 import { AuthContext } from './context/AuthContext';
 import { AppRouter } from './routers/AppRouters';
 import { authReducer } from './reducers/authReducers';
+=======
+
+import UserProfile from '../pages/UserProfile'
+>>>>>>> 63d0da337d466e06048eb5ee9055f9c54625828d
 
 function App() {
   const infoContextUser = {
-    isLogged : true,
+    isLogged : false,
     userName : "PepeGuapo"
   }
   const initialRoleXD = {
@@ -14,13 +19,20 @@ function App() {
   }
   const [roleXD, dispatch] = useReducer(authReducer, initialRoleXD);
 
+<<<<<<< HEAD
+  const [logged, dispatchLogged] = useReducer(authReducer, infoContextUser);
+
   return (
     <AuthContext.Provider value ={{
       infoContextUser,
-      roleXD, dispatch
+      roleXD, dispatch,
+      logged, dispatchLogged
     }}>
       <AppRouter/>
     </AuthContext.Provider>
+=======
+    <UserProfile/>
+>>>>>>> 63d0da337d466e06048eb5ee9055f9c54625828d
     
   )
 }
